c645cb749d797fc286cdbdbde30cc405db572f69
Andres Gomez Buitrago <agomezbuitrago9@gmail.com>
2025-01-26 16:24:42 -0500

    CRUD completo

--- api.js
+++ api.js
@@ -1,11 +1,11 @@
-var mongoose = require("mongoose");
-var express = require("express");
-var router = express.Router();
-var TaskModel = require('./task_schema');
+import { connect } from "mongoose";
+import { Router } from "express";
+var router = Router();
+import TaskModel, { find, updateOne, deleteOne } from './task_schema';
 
 let environment = null;
 
-if (!process.env.ON_HEROKU) {
+if (!process.env.ON_RENDER) {
     console.log("Cargando variables de entorno desde archivo");
     const env = require('node-env-file');
     env(__dirname + '/.env');
@@ -22,9 +22,9 @@ var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMON
 
 const db = (query);
 
-mongoose.Promise = global.Promise;
+Promise = global.Promise;
 
-mongoose.connect(db, {
+connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function (error) {
@@ -59,7 +59,7 @@ router.post('/create-task', function (req, res) {
 });
 
 router.get('/all-tasks', function (req, res) {
-    TaskModel.find(function (err, data) {
+    find(function (err, data) {
         if (err) {
             res.status(500).send("Internal error\n");
         }
@@ -70,7 +70,7 @@ router.get('/all-tasks', function (req, res) {
 });
 
 router.post('/update-task', function (req, res) {
-    TaskModel.updateOne({ TaskId: req.body.TaskId }, {
+    updateOne({ TaskId: req.body.TaskId }, {
         Name: req.body.Name,
         Deadline: req.body.Deadline
     }, function (err, data) {
@@ -83,7 +83,7 @@ router.post('/update-task', function (req, res) {
 });
 
 router.delete('/delete-task', function (req, res) {
-    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+    deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
         if (err) {
             res.status(500).send("Internal error\n");
         } else {
@@ -92,4 +92,4 @@ router.delete('/delete-task', function (req, res) {
     });
 });
 
-module.exports = router;
+export default router;
